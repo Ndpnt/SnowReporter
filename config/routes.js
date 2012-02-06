38775113a4bc90c84fd8be14cbd52a1f38e7b650
Nicolas Dupont <npg.dupont@gmail.com>
2012-02-06 21:24:41 +0100

    restrictions method

--- config/routes.js
+++ config/routes.js
@@ -2,8 +2,19 @@ var users = require('../app/controllers/users_controller');
 var hills = require('../app/controllers/hills_controller');
 
 module.exports = function(app) {
-	app.get('/hills/' 	 	, function(req, res) { hills.index(req, res) });
-	app.get('/hills/:name'  , function(req, res) { hills.show(req, res)  });
+	function restrict(req, res, next) {
+	  if (req.loggedIn) {
+	  	next();
+	  } else {
+	    req.session.error = 'Access denied!';
+		console.log("\n" + '***************************************');
+		console.log(req.session.error);
+		console.log('***************************************' + "\n");
+	    res.redirect('/users/login');
+	  }
+	}
 	
+	app.get('/hills/' 	 	, restrict, function(req, res) { hills.index(req, res) });
+	app.get('/hills/:name'  , function(req, res) { hills.show(req, res)  });
 	app.get('/'		  	 	, function(req, res) { users.index(req, res) });
 }
